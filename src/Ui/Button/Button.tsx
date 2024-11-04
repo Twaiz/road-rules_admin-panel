b7329838abe50743e26db292d6ba97124cc8a7a2
Bohdan Khomenko <homenkobodya@gmail.com>
2024-11-04 11:16:41 +0300

    add[component Button]: logic component

---
+++ src/Ui/Button/Button.tsx
@@ -0,0 +1,59 @@
+import style from './Button.module.scss';
+
+import Loader from '../Loader';
+
+type ButtonTypes = 'button' | 'submit';
+type ButtonStyles = 'button' | 'link';
+
+interface IButton {
+  text: string;
+  buttonStyle?: ButtonStyles;
+  type?: ButtonTypes;
+  disabled?: boolean;
+  onChange?: () => void;
+}
+
+const Button = (props: IButton) => {
+  const {
+    text,
+    buttonStyle = 'button',
+    type = 'button',
+    disabled = false,
+    onChange,
+  } = props;
+
+  //? Я считаю, тут лучше сразу же сделать так, чтобы потом не переписывать ?\\
+  const getButtonStyles = () => {
+    switch (buttonStyle) {
+      case 'link':
+        return style.link;
+      default:
+        return style.button;
+    }
+  };
+
+  const isLoading = false; //? Позже тут будет динамическое состояние ?\\
+  const isSubmitType = type === 'submit';
+  const styleButtonDisabled = style.button__disabled;
+
+  const combinedClassname = [
+    getButtonStyles,
+    isSubmitType && isLoading && styleButtonDisabled,
+    disabled && styleButtonDisabled,
+  ]
+    .filter(Boolean) //? Убирает все ложные значения из массива ?\\
+    .join(' ');
+
+  return (
+    <button
+      className={combinedClassname}
+      type={type}
+      disabled={disabled}
+      onChange={onChange}
+    >
+      {isLoading ? <Loader /> : text}
+    </button>
+  );
+};
+
+export default Button;
