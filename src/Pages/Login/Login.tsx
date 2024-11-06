77a18e3dca53c50579b6ae93f8c0c201a0a2b4fe
Bohdan Khomenko <homenkobodya@gmail.com>
2024-11-06 20:25:58 +0300

    add[component Login]: added component Login that deleted bug

---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,31 @@
+import { Form } from 'react-router-dom';
+
+import style from './Login.module.scss';
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <h2>Логин</h2>
+        <p>Войдите в свой аккаунт</p>
+      </div>
+
+      <Form method="POST">
+        <div>
+          <input />
+          <div>
+            <input />
+            <input type="checkbox" />
+          </div>
+        </div>
+      </Form>
+
+      <div className={style.actions}>
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
