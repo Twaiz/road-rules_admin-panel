47df735a11cd572b8850083e4ba8798fdf0942a9
Bohdan Khomenko <homenkobodya@gmail.com>
2024-11-10 20:26:54 +0200

    fic[export cycle]: fixed bugs with export localStorageSelectors

--- src/Tools/index.tsx
+++ src/Tools/index.tsx
@@ -1,3 +1,3 @@
-export { ToggleTheme } from './ThemeToogle/ToogleTheme';
 export { localStorageSelectors } from './Helpers/localStorageSelectors';
+export { ToggleTheme } from './ThemeToogle/ToogleTheme';
 export { handleAsyncOperation } from './Helpers/handleAsyncOperation';
